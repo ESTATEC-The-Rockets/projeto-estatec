320f30a8f1403fe7061d9a80942ef8c7e7bd7fb4
z4mbon1 <dev.lais.zamboni@gmail.com>
2026-03-20 15:51:47 -0300

    anotações na entidade Estacionamento

--- backend/estatec/src/main/java/br/com/estatec/api/entities/Estacionamento.java
+++ backend/estatec/src/main/java/br/com/estatec/api/entities/Estacionamento.java
@@ -2,17 +2,24 @@ package br.com.estatec.api.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
-@Table (name = "estacionamento")
+@Table(name = "estacionamento")
 public class Estacionamento {
 
+	@Pattern(regexp = "^[\\p{L}]+( [\\p{L}]+)*$", message = "O nome do estacionamento deve conter apenas letras e espaços.")
+	@NotBlank(message = "O nome do estacionamento é obrigatório.")
 	private String nomeEstacionamento;
-	private int qtdVagas;
 	
+	@NotNull(message = "A quantidade de vagas é obrigatória.")
+	private int qtdVagas;
+
 	public Estacionamento() {
 	}
-	
+
 	public Estacionamento(String nomeEstacionamento, int qtdVagas) {
 		this.nomeEstacionamento = nomeEstacionamento;
 		this.qtdVagas = qtdVagas;
--- backend/estatec/src/main/java/br/com/estatec/api/entities/Usuario.java
+++ backend/estatec/src/main/java/br/com/estatec/api/entities/Usuario.java
@@ -25,75 +25,72 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table (name = "tb_usuario")
+@Table(name = "tb_usuario")
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Pattern(
-			regexp = "^[\\p{L}]+( [\\p{L}]+)*$",  message = "O nome do usuário deve conter apenas letras e espaços.") 
-	@NotBlank(message = "O nome é obrigatório.")
+
+	@Pattern(regexp = "^[\\p{L}]+( [\\p{L}]+)*$", message = "O nome do usuário deve conter apenas letras e espaços.")
+	@NotBlank(message = "O nome do usuário é obrigatório.")
 	protected String nome;
-	
+
 	@NotBlank(message = "O RG é obrigatório.")
-	@Pattern(regexp = "^[A-Za-z0-9.-]+$", 
-			 message = "RG deve conter apenas números, letras, ponto e hífen.")			
+	@Pattern(regexp = "^[A-Za-z0-9.-]+$", message = "RG deve conter apenas números, letras, ponto e hífen.")
 	@Size(min = 7, max = 12, message = "RG deve ter pelo menos 7 dígitos.")
 	@Column(nullable = false, unique = true, length = 12)
 	protected double rg;
-	
+
 	@NotBlank(message = "O CPF é obrigatório.")
 	@CPF(message = "O CPF é inválido.")
 	@Column(nullable = false, unique = true, length = 14)
 	protected double cpf;
-	
+
 	@NotNull(message = "A data de nascimento é obrigatória.")
 	@Past(message = "Data de nascimento deve estar no passado.")
 	protected LocalDate dataNascimento;
-	
+
 	@Email(message = "E-mail inválido.")
 	@Size(max = 120, message = "E-mail deve ter no máximo 120 caracteres.")
 	@Column(unique = true, length = 120)
 	protected String email;
-	
+
 	@NotBlank(message = "A senha é obrigatória.")
-	@Pattern(
-			regexp = "^[A-Za-z0-9_@#]+$",
-			message = "A senha deve conter apenas letras, números, underline, arroba e hashtag.")
+	@Pattern(regexp = "^[A-Za-z0-9_@#]+$", message = "A senha deve conter apenas letras, números, underline, arroba e hashtag.")
 	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres.")
 	protected String senha;
-	
+
 	@TelefoneBR(message = "Telefone deve estar no padrão brasileiro.")
 	@Column(length = 20, unique = true)
 	@NotBlank(message = "O telefone é obrigatório.")
 	protected String telefone;
-	
+
 	@NotNull(message = "O carro é obrigatório.")
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "fk_carro", nullable = false, unique = true)
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	protected Carros carro;
-	
+
 	@NotNull(message = "O dono do carro é obrigatório.")
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "fk_dono", nullable = false, unique = true)
 	@JsonManagedReference
 	protected DonoCarro dono;
-	
+
 	public Usuario() {
-		
+
 	}
-	
-	public Usuario(String nome, double rg, double cpf, LocalDate dataNascimento, String email, String senha, String telefone) {
+
+	public Usuario(String nome, double rg, double cpf, LocalDate dataNascimento, String email, String senha,
+			String telefone) {
 		this.nome = nome;
 		this.rg = rg;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
 		this.email = email;
 		this.senha = senha;
-		this. telefone = telefone;
+		this.telefone = telefone;
 	}
 
 	public String getNome() {
