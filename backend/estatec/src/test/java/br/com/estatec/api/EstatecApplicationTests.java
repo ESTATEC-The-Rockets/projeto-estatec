65ee20ee6349a0ccb8647e9b97540675a22e7631
MariaVitorino09 <dev.maria.vitorino@gmail.com>
2026-03-20 09:48:25 -0300

    commit inicial

---
+++ backend/estatec/src/test/java/br/com/estatec/api/EstatecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estatec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
