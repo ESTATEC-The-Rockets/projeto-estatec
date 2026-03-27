667e5506bac177db077d93304aa5144d358f3438
z4mbon1 <dev.lais.zamboni@gmail.com>
2026-03-27 15:25:50 -0300

    atualizações
    Carros controller/service e documentos (ainda n estão prontos)

---
+++ backend/estatec/src/main/java/br/com/estatec/api/entities/Documentos.java
@@ -0,0 +1,5 @@
+package br.com.estatec.api.entities;
+
+public class Documentos {
+
+}
--- backend/estatec/src/main/java/br/com/estatec/api/enums/Cor.java
+++ backend/estatec/src/main/java/br/com/estatec/api/enums/Cor.java
@@ -1,4 +1,4 @@
-package br.com.estatec.api.enuns;
+package br.com.estatec.api.enums;
 
 public enum Cor {
 	BRANCO,
