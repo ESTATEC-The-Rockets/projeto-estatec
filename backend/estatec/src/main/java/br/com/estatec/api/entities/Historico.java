e60273d80747751d869adb3df7fbba1d5f8c35f0
dev-murilo-castro <dev.murilo.castro@gmail.com>
2026-05-13 13:40:16 -0300

    Histórico com as anotações (nao esta completo ainda)

--- backend/estatec/src/main/java/br/com/estatec/api/entities/Historico.java
+++ backend/estatec/src/main/java/br/com/estatec/api/entities/Historico.java
@@ -1,12 +1,16 @@
 package br.com.estatec.api.entities;
 
-import java.sql.Time;
+import java.time.LocalTime;
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.PastOrPresent;
 
 @Entity
 @Table(name = "tb_historico") 
@@ -16,27 +20,41 @@ public class Historico {
 	@GeneratedValue
 	private Long id;
 	
+	@OneToMany
+	@JoinColumn(name = "fk_dono_carro")
 	private DonoCarro donoCarro;
 	
+	@OneToMany
+	@JoinColumn(name = "fk_estacionamento")
 	private Estacionamento estacionamento;
 	
+	@PastOrPresent
+	@Column(name = "dia_do_historico")
 	private Date date; 	
 	
-	private Time horarioEntrada;
+	@Column(name = "horario_entrada")
+	private LocalTime horarioEntrada;
+
+	@Column(name = "horario_saida")
+	private LocalTime horarioSaida;
 	
-	private Time horarioSaida;
+	@Column(name = "calculo_entrada")
+	private boolean entrada;
+	
+	
 	
 	
 	public Historico(){
 		
 	}
 	
-	public Historico(Long id, DonoCarro donoCarro, Estacionamento estacionamento, Date date, Time horarioEntrada, Time horarioSaida) {
+	public Historico(Long id, DonoCarro donoCarro, Estacionamento estacionamento, Date date, LocalTime horarioEntrada, LocalTime horarioSaida, boolean entrada) {
 		this.date = date;
 		this.donoCarro = donoCarro;
 		this.estacionamento = estacionamento;
 		this.horarioEntrada = horarioEntrada;
 		this.horarioSaida = horarioSaida;
+		this.entrada = entrada;
 		
 	}
 
@@ -72,19 +90,19 @@ public class Historico {
 		this.date = date;
 	}
 
-	public Time getHorarioEntrada() {
+	public LocalTime getHorarioEntrada() {
 		return horarioEntrada;
 	}
 
-	public void setHorarioEntrada(Time horarioEntrada) {
+	public void setHorarioEntrada(LocalTime horarioEntrada) {
 		this.horarioEntrada = horarioEntrada;
 	}
 
-	public Time getHorarioSaida() {
+	public LocalTime getHorarioSaida() {
 		return horarioSaida;
 	}
 
-	public void setHorarioSaida(Time horarioSaida) {
+	public void setHorarioSaida(LocalTime horarioSaida) {
 		this.horarioSaida = horarioSaida;
 	}
 	
