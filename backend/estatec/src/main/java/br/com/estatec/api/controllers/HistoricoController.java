80833051735cd556a856d6587a9af37cf84f9eca
dev-murilo-castro <dev.murilo.castro@gmail.com>
2026-05-15 12:53:11 -0300

    Controller do Historico atualizado com sucesso, contendo apenas o necessario que um Historico necessita.

--- backend/estatec/src/main/java/br/com/estatec/api/controllers/HistoricoController.java
+++ backend/estatec/src/main/java/br/com/estatec/api/controllers/HistoricoController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,25 +47,4 @@ public class HistoricoController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(novoHistorico);
 	}
 	
-	@PutMapping("/{id}")
-	public ResponseEntity<Historico> atualizar(@PathVariable Long id, @Valid @RequestBody Historico historico) {
-		Historico historicoAtualizado = service.atualizar(id, historico);
-		if (historicoAtualizado != null) {
-			return ResponseEntity.ok(historicoAtualizado);
-		}
-		return ResponseEntity.notFound().build();
-	}
-	
-	@DeleteMapping ("/{id}")
-	public ResponseEntity<Object> deletar(@PathVariable Long id){
-		Optional<Historico> historico = service.buscarPorId(id);
-		
-		if(historico.isPresent()) {
-			service.deletar(id);
-			return ResponseEntity.status(HttpStatus.OK)
-					.body("Sucesso: O aluno foi excluido permanentemente");
-		}
-		return ResponseEntity.status(HttpStatus.OK)
-				.body("Erro: Não é possível deletar. O aluno com ID " + id + " não foi encontrado.");
-	}
 }
